72bbab5d71b1752a9f780d7707f28c8cb0857c6a
bmrickman <brandon.m.rickman@gmail.com>
2021-03-31 14:12:47 -0700

    enhancing DB module with decorator to simplify use of connect, method, disconnect pattern

--- src/db.py
+++ src/db.py
@@ -7,58 +7,68 @@ from mysql_cfg import mysql_cfg
 
 
 class db:
+
     def __init__(self):
         self.mysql_cfg = mysql_cfg
+        self.query = None
+        self.cnx = None
+        self.cursor = None
+        self.row = None
 
+    # Used to wrap all methods in connect -> method -> disconnect pattern
+    def try_connect(method):
+        def inner(self,*params):
+            try:
+                self.cnx = mysql.connector.connect(**self.mysql_cfg)
+                self.cursor = self.cnx.cursor()
+                res = method(self, *params)
+                self.cnx.close()
+                return(res)
+
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                    print("Something is wrong with your user name or password")
+                elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                    print("Database does not exist")
+                # elif my errors - handled here - but how setup error codes with mysql.connector.errorcode?
+                else:
+                    print(err)
+        return inner
+
+    @try_connect
+    def runSQLFile(self, fileHandle):
+        self.fileHandle = fileHandle
+        result_iterator = self.cursor.execute(fileHandle.read(), multi=True)
+        for res in result_iterator:
+            print("Running query: ", res)  # Will print out a short representation of the query
+            print(f"Affected {res.rowcount} rows")
+        self.cnx.commit()
+
+    @try_connect
     def runQuery(self, query):
-        try:
-            self.query = query
-            self.cnx = mysql.connector.connect(**self.mysql_cfg)   # attempt connect
-            self.cursor = self.cnx.cursor()
-            self.cursor.execute(query)                              # Query execution
-            self.rows = self.cursor.fetchall()
-            self.cnx.close()                                        # Close connection
+        self.query = query
+        self.cursor.execute(query)                                    # Query execution
+        self.rows = self.cursor.fetchall()
 
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            # elif my errors - handled here - but how setup error codes with mysql.connector.errorcode?
-            else:
-                print(err)
-
-    # 03/28/2021 - New, may be broken
-    def runSQLFile(self, filePath):
-        self.filePath = filePath
-        self.cnx = mysql.connector.connect(**self.mysql_cfg)  # attempt connect
-        self.cursor = self.cnx.cursor()
-
-        with open(filePath, 'r') as f:
-            result_iterator = self.cursor.execute(f.read(), multi=True)  # returns immediately, but we need a proise that things are complete, don't we?
-
-            for res in result_iterator:
-                print("Running query: ", res)  # Will print out a short representation of the query
-                print(f"Affected {res.rowcount} rows")
-
-            self.cnx.commit()
-        self.cnx.close()
-
-
-            # should be O(n)
-    def rowListToRowDict(self):
-        columns = [d[0] for d in self.cursor.description]
-        for i, row in enumerate(self.rows):
-            self.rows[i] = dict(zip(columns, row))
-
+    @try_connect
     def fetchAll(self, query):
         # If queries get too big to hold in python memory, we can store results on DB side and pull ass needed using fetchMany
         # https://www.mysqltutorial.org/python-mysql-query/
         self.runQuery(query)
         self.rowListToRowDict()
+        return(self.rows)
 
+    @try_connect
+    def executemany(self, stmt, rows):
+        self.cursor.executemany(stmt, rows)
+
+    def rowListToRowDict(self):
+        columns = [d[0] for d in self.cursor.description]
+        for i, row in enumerate(self.rows):
+            self.rows[i] = dict(zip(columns, row))
 
 
 db = db()
+
 
 
--- src/tests/db_test.py
+++ src/tests/db_test.py
@@ -1,10 +1,20 @@
+## Db object is one directory higher
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-
+## import db object
 from db import db
-db.runSQLFile('../dummy_db_init_script.sql')
+
+## Drop the database
+db.runQuery('DROP DATABASE IF EXISTS sc')
+## Pass .SQL file to create database
+with open('../db_init_shallow_dvd_sc.sql', 'r') as file:
+    db.runSQLFile(file)
+# Calling procedure to rebuild database
+db.runQuery('CALL sc.build_shallow_vg_sc(10,10,10)')
+# Pulling data from DB
 db.fetchAll("SELECT * FROM sc.products")
+# Should display only if all above processes worked
 print(db.rows)
